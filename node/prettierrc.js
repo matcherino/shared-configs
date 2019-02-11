2152685eb6f6d96f805e966758e0b7273ede17c7
mgutz <mario@matcherino.com>
2019-02-10 18:19:01 -0800

    use semicolons

--- node/prettierrc.js
+++ node/prettierrc.js
@@ -3,7 +3,7 @@ module.exports = {
   bracketSpacing: false,
   jsxBracketSameLine: false,
   parser: 'babylon',
-  semi: false,
+  semi: true,
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'es5',
